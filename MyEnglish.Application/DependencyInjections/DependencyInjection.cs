e203cd5b3bd733aac72803623de2f5d1c8e8e053
LucTran <luctd2209@gmail.com>
2025-11-06 22:17:15 +0700

    Init source code: Applicaiton layer (60%)

---
+++ MyEnglish.Application/DependencyInjections/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MyEnglish.Application.DependencyInjections
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            // Add application services here
+        }
+    }
+}
